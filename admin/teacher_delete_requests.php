cd5e3eb09df6d047b4ba4536a45efbb988506731
ufeslm <123030312+ufeslm@users.noreply.github.com>
2024-05-06 16:36:38 +0100

    lol

--- admin/teacher_delete_requests.php
+++ admin/teacher_delete_requests.php
@@ -22,11 +22,34 @@ $result = mysqli_query($conn, $query);
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php
+    require_once "dashbord_head.html";
+  ?>
   <title>Delete Requests</title>
+  <style>
+    .card {
+      max-width: 900px;
+      margin: 50px auto;
+      padding: 20px;
+      border: 1px solid #ccc;
+      border-radius: 5px;
+      box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+    }
+    </style>
 
 </head>
 <body>
   <div class="container">
+  <?php
+    require_once "dashbord_body.html";
+    ?>
+    <div class="main">
+      <div class="topbar">
+        <div class="toggle">
+          <ion-icon name="menu-outline"></ion-icon>
+        </div>
+      </div>
+      <div class="card">
     <h2>Delete Requests</h2>
     <table>
       <thead>
@@ -54,8 +77,9 @@ $result = mysqli_query($conn, $query);
         ?>
       </tbody>
     </table>
-
-    <a href="welcome.php">Back to Dashboard</a>
   </div>
 </body>
+<?php
+  require_once "dashboard_script.html";
+?>
 </html>
